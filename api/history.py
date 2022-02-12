b130697b114ebfac68075be330010fca77b14f67
vadim-ivlev <vadim.ivlev@gmail.com>
2022-02-12 20:36:22 +0300

    fix: help

--- api/history.py
+++ api/history.py
@@ -24,7 +24,23 @@ class handler(BaseHTTPRequestHandler):
         interval = query.get("interval",["1m"])[0]
         print(symbol, period, interval)
         data = self.yf_history(symbol, period, interval)
-        self.wfile.write(json.dumps(data).encode('utf-8'))
+        if len(data)>0:
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+        else :                    
+            s = """ 
+            Ticker historical data.
+
+            Args:
+                symbol (str, optional): A ticker symbol. Defaults to "MSFT".
+                period (str, optional): Valid periods are: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max. Defaults to '1d'.
+                interval (str, optional): valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo. Defaults to '1m'.
+
+            Returns:
+                list: list of dictionaries with fields t,o,h,l,c
+            """
+            print("no data")
+            self.wfile.write(json.dumps({"params":s}).encode('utf-8'))
+            
         return
     
 
